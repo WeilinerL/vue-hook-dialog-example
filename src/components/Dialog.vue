7974729f0cde545301b6fc2d9d34d59ec463c109
WeilinerL <52592562+WeilinerL@users.noreply.github.com>
2023-07-27 20:56:53 +0800

    Initial commit

---
+++ src/components/Dialog.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{ visible: boolean }>();
+const emit = defineEmits(['close']);
+
+const close = () => {
+  emit('close');
+};
+</script>
+
+<template>
+  <div class="dialog" v-if="visible">
+    <div class="mask"></div>
+    <div class="content">
+      <h3>标题</h3>
+      <div>内容</div>
+      <footer>
+        <button @click="close">关闭</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.dialog {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .mask {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: #ccc;
+    opacity: 0.8;
+    z-index: -1;
+  }
+  .content {
+    background: #fff;
+    border-radius: 20px;
+    padding: 20px;
+    min-width: 300px;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Dialog from './Dialog.vue';
+import useCommandComponent from './useCommandComponent';
+
+defineProps<{ msg: string }>();
+
+const MyDialog = useCommandComponent(Dialog);
+
+const handleDialog = () => {
+  MyDialog({
+    title: '父组件弹窗',
+    dialogVisible: true,
+    onCancel: () => MyDialog.close(),
+  });
+};
+</script>
+
+<template>
+  <h3>click to open dialog</h3>
+  <button @click="handleDialog">open</button>
+</template>
+
+<style scoped></style>
